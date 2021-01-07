2f8ba03e60d765049b54572ac6503fe35edb7cff
eslaamehab <eslaam.ehaab@gmail.com>
2021-01-08 01:49:36 +0200

    dmks

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,28 +1,7 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
-//import logo from './logo.svg';
-import './App.css';
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import loginInfo from "./components/loginInfo.component";
@@ -37,7 +16,7 @@ function App() {
       <Route path = "/register" exact component = {regInfo}/>
       <Route path = "/profile" exact component = {profile}/>
     </Router>
->>>>>>> master
+
   );
 }
 
