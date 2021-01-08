7f284bf752132318a8c7aec3ea45c868d9bb2c22
eslaamehab <eslaam.ehaab@gmail.com>
2021-01-08 02:02:34 +0200

    n

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,10 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-<<<<<<< HEAD
-=======
 
->>>>>>> master
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
